681cb32f675619d5db36721c970ac5acae272115
work <smashleader666@gmail.com>
2025-01-31 22:03:51 +0300

    Models added

---
+++ DealSpot/main/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.urls import reverse
+
+class Category:
+    name = models.CharField(max_length=30,
+                            unique=True)
+    slug = models.SlugField(max_length=30,
+                            unique=True)
+    
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['name'])]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=75)
+    slug = models.SlugField(max_length=75)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    available = models.BooleanField(default=True)
+    crated = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    discount = models.DecimalField(max_digits=4, default=0, decimal_places=2)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('main:product_detail', args=[self.slug])
