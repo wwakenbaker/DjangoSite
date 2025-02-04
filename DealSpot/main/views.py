132598e047015b99252e7766a7641ea557199860
work <smashleader666@gmail.com>
2025-02-04 16:27:06 +0300

    Added pagination

--- DealSpot/main/views.py
+++ DealSpot/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from.models import Category, Product
 
 
@@ -15,3 +16,22 @@ def product_detail(request, slug):
     return render(request,
                   'main/product/detail.html',
                   {'product': product})
+
+def product_list(request, category_slug=None):
+    page = request.GET.get('page', 1)
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    paginator = Paginator(products, 1)
+    current_page = paginator.page(int(page))
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        paginator = Paginator(products.filter(category=category), 1)
+        products = products.filter(category=category)
+        current_page = paginator.page(int(page))
+    return render(request,
+                  'main/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': current_page,
+                   'slug_url': category_slug})
