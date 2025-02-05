b1dd059be22dfb864ad72d0ac81c429fce4307b0
work <smashleader666@gmail.com>
2025-02-06 01:15:32 +0300

    price

--- DealSpot/cart/views.py
+++ DealSpot/cart/views.py
@@ -15,7 +15,7 @@ def cart_add(request, product_id):
         cart.add(product=product,
                  quantity=cd['quantity'],
                  override_quantity=cd['override'])
-    return redirect('cart:cart_detail')
+    return redirect('main:product_list')
 
 @require_POST
 def cart_remove(request, product_id):
